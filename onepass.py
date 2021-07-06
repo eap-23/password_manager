8334f94fca561d223c2fffad40ef848903a158a8
Eric Pusey <puseye23@gmail.com>
2021-07-06 18:26:55 -0400

    Added iteration for website keys
    User now prompted for number of keys they want to add. New key object created for each iteration.

--- onepass.py
+++ onepass.py
@@ -4,24 +4,33 @@ from key import Key
 
 def createTable(c):    
     c.execute("""CREATE TABLE keys (
-                website_key text,
+                website_key text primary key,
                 username text,
                 password text
                 )""")
 
 def generateKeys():
     #Need to make this an iteration!
-    website_key = input('Enter Key Name: ')
-    username = input('Enter username used for website: ')
-    password = input('Enter password: ')
+    numKeys = int(input('Enter Number of Keys: '))
+    print()
+    keys = []
     
-    key_1 = Key(website_key, username, password)
+    for i in range(0, numKeys):             
+        website_key = input('Enter Key Name: ')
+        username = input('Enter username used for website: ')
+        password = input('Enter password: ')
+        print()
     
-    return key_1
+        key = Key(website_key, username, password)
+        
+        keys.append(key)
+    
+    return keys
 
-def insertKey(key, conn, c):
-    with conn:
-        c.execute("INSERT INTO keys VALUES (:website_key, :username, :password)", 
+def insertKey(keys, conn, c):
+    for key in keys:
+        with conn:
+            c.execute("INSERT INTO keys VALUES (:website_key, :username, :password)", 
                   {'website_key':key.website_key, 'username':key.username, 
                    'password':key.password})
     
@@ -30,12 +39,12 @@ def main():
     c = conn.cursor()
     
     createTable(c)
-    key_1 = generateKeys()
-    insertKey(key_1, conn, c)
+    keys = generateKeys()
+    insertKey(keys, conn, c)
     
     c.execute("SELECT * FROM keys WHERE password='test_pw'")
     
-    print(c.fetchone())
+    print(c.fetchall())
     
     conn.commit()
     conn.close()
