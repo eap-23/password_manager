6d2915debab6498780607327893f5279dc366c41
Eric Pusey <puseye23@gmail.com>
2021-07-06 19:34:26 -0400

    Added menu and deleteKey functions

--- onepass.py
+++ onepass.py
@@ -2,6 +2,19 @@ import os.path
 import sqlite3
 from key import Key
 
+def menu():
+    print("\n***Onepass - Password Manager***\n")
+    
+    print('(a)dd')
+    print('(d)elete')
+    print('(v)iew')
+    print('(c)opy')
+    print('(q)uit')
+    
+    selected = input("\n>>> ").lower()
+    
+    return selected
+           
 def createTable(c):    
     c.execute("""CREATE TABLE keys (
                 website_key text primary key,
@@ -32,17 +45,36 @@ def insertKey(keys, conn, c):
         with conn:
             c.execute("INSERT INTO keys VALUES (:website_key, :username, :password)", 
                   {'website_key':key.website_key, 'username':key.username, 
-                   'password':key.password})
-    
+                   'password':key.password})  
+            
+def deleteKey(website_key, conn, c):
+     with conn:
+         c.execute("DELETE from keys WHERE website_key = :website_key",
+                   {'website_key': website_key})
+          
 def main():
-    conn = sqlite3.connect(':memory:')
+    conn = sqlite3.connect('test.db')
     c = conn.cursor()
     
-    createTable(c)
-    keys = generateKeys()
-    insertKey(keys, conn, c)
+    # createTable(c)
+    selected = menu()
     
-    c.execute("SELECT * FROM keys WHERE password='test_pw'")
+    if selected == "a":   
+        keys = generateKeys()
+        insertKey(keys, conn, c)
+    elif selected == "d":
+        website_key = input("Enter Key to Delete: ")
+        deleteKey(website_key, conn, c)
+    elif selected == "c":
+        pass
+    elif selected == "q":
+        pass
+    
+    # insertKey(keys, conn, c)
+    
+    # c.execute("SELECT * FROM keys WHERE password='test'")
+    
+    c.execute("SELECT * FROM keys")
     
     print(c.fetchall())
     
